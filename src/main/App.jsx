c02377575c902f2be826e1d7a1e310666b7cf7df
LucasPietro <lucaspietrovieira@hotmail.com>
2019-09-02 13:46:24 -0300

    First Commit

---
+++ src/main/App.jsx
@@ -0,0 +1,17 @@
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
+import '../template/custom.css'
+import React from 'react'
+import Menu from '../template/menu'
+import Routes from './routes'
+
+function App() {
+  return (
+    <div className="container">
+      <Menu />
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
